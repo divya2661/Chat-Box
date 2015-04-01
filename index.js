ae1d253a88cbe8706d55b2916225199d83ed5e3f
divya2661 <divn2661@gmail.com>
2015-04-02 03:18:37 +0530

    added multi room functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var port = 3700;
 var usernames = {};
+var rooms = ['room1','room2','room3'];
 //app.set	('views',__dirname + '/tpl');
 //app.set('view engine', "jade");
 //app.engine('jade', require('jade').__express);
@@ -19,7 +20,7 @@ io.sockets.on('connection',function(socket){
 	
 	
 	socket.on('send',function(data){
-		io.sockets.emit('message',data);
+		io.sockets.to(socket.room).emit('message',data);
 	});
 
 
@@ -27,15 +28,29 @@ io.sockets.on('connection',function(socket){
 		console.log('yeah it working');
 		console.log(username);
 		socket.username = username;
+		socket.room = 'room1';
 		usernames[username] = username;
-		
-		socket.emit('message', {message: 'you have connected'});
-		socket.broadcast.emit('message',{message: username +' has connected.'});
+		socket.join('room1');		
+		socket.emit('message', {message: 'you have connected to room1'});
+
+		socket.broadcast.to('room1').emit('message',{message: username +' has connected.'});
 		
 		io.sockets.emit('updateusers',usernames);
+		io.sockets.emit('updateRoom',rooms,'room1');
 
 	});
 
+	socket.on('switchRoom',function(newroom){
+		console.log("its fine..............222");
+
+		socket.leave(socket.room);
+		socket.join(newroom);
+
+		socket.emit('message',{message:'you have conected to ' + newroom});
+		socket.broadcast.to(newroom).emit('message',{message: socket.username + 'has conected to this room'});
+		socket.emit('updateRoom',rooms,newroom);
+
+	})
 
 
 
